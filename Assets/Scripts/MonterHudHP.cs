460cee823a00fe1865897277fd9dad100b68799f
Munkittaja <joni.kurki@gmail.com>
2017-07-29 19:28:06 +0300

    added monster hp bar. Doesnt scale yet. Shows 100% of health only.

---
+++ Assets/Scripts/MonterHudHP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonterHudHP : MonoBehaviour {
+
+    public float barWidth;
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(3f, 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
